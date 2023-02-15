049de4a47944204045cd2df6c17f6cbde486f2ad
jacobsilcoff <jacob.silcoff@mail.mcgill.ca>
2023-02-14 19:18:04 -0500

    Finished basic code required to parse the pdf & create anki deck

--- card_generator.py
+++ card_generator.py
@@ -1,6 +1,7 @@
 import openai
 import requests
 import os
+from doc_parser import create_chunks
 
 
 def read_file(filename):
@@ -14,14 +15,33 @@ prompt = read_file("prompt.txt")
 
 def generate_flashcards(information):
     text = prompt + "\n" + information + "\n\n Output:\n"
-    completions = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=text,
-        max_tokens=4096 - int(len(text) / 3),  # this should be fixed
-        n=1,
-        stop=None,
-        temperature=.1,
-    )
+    try:
+        completions = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=text,
+            max_tokens=4096 - int(len(text) / 3),  # this should be fixed
+            n=1,
+            stop=None,
+            temperature=.1,
+        )
+    except openai.error.InvalidRequestError as err:
+        print(err)
+        m = err.error.message
+        try:
+            prompt_tokens = int(m[m.index("(") + 1:].split(' ')[0])
+        except Exception as e:
+            print(str(e))
+            raise err
+
+        completions = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=text,
+            max_tokens=4096 - prompt_tokens,  # this should be fixed
+            n=1,
+            stop=None,
+            temperature=.1,
+        )
+
     lines = completions.choices[0].text.strip().split("\n")
     result = []
     question = None
@@ -64,7 +84,6 @@ def add_card_basic(question, answer, deck_name="AI IM Deck"):
 
 def make_cards_from_chunk(filename):
     directory = "generated_cards"
-    text = ""
     with open(f"chunks/{filename}.txt") as f:
         text = f.read()
     if not text:
@@ -76,8 +95,34 @@ def make_cards_from_chunk(filename):
         f.write("\n".join([f"Q:{q}\nA:{a}\n" for q, a in cards]))
 
 
-# flashcards = generate_flashcards(prompt + "\n" + information + "\n\n Output:\n")
-# for q, a in flashcards:
-#     make_card_basic(q, a)
+def process_all_chunks(skip_existing=True):
+    files = os.listdir("chunks")
+    existing = [x[:-10] for x in os.listdir("generated_cards")]
+    for file in files:
+        if skip_existing and file[:-4] in existing:
+            print("SKIPPING " + file)
+            continue
+        print("starting on cards for " + file)
+        make_cards_from_chunk(file[:-4])
+        print("created card file for " + file + "\n")
 
-make_cards_from_chunk("Asthma")
+
+def make_cards_from_files():
+    files = os.listdir("generated_cards")
+    qs = []
+    for file in files:
+        with open(f"generated_cards/{file}", "r") as f:
+            lines = f.readlines()
+            for i in range(0, len(lines), 3):
+                try:
+                    qs.append((lines[i][2:], lines[i + 1][2:]))
+                except Exception as e:
+                    print(e)
+    for q, a in qs:
+        add_card_basic(q, a)
+
+
+def __main__():
+    create_chunks()
+    process_all_chunks()
+    make_cards_from_files()
